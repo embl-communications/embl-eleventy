29785cad8b1ff49cb6bc83921a2a8fb239e6b88a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-12 09:29:15 +0000

    Deploy embl-communications/embl-eleventy to github.com/embl-communications/embl-eleventy.git:gh-pages

---
+++ _data/siteConfig.js
@@ -0,0 +1,10 @@
+// Some various reusable configuration
+module.exports = {
+  siteInformation: {
+    title: "EMBL Eleventy boilerplate",
+    short_description: 'This allows using the <a class="vf-link" href="https://www.11ty.io">11ty</a> static site generator with direct access to <a class="vf-link" href="https://github.com/visual-framework/vf-core">Visual Framework 2.0</a> components.',
+    url: "https://embl-communications.github.io/embl-eleventy/",
+    author: "EMBL Digital team",
+    email: "ken.hawkins@embl.de",
+  }
+};
--- assets/vf-hero/vf-hero.js
+++ assets/vf-hero/vf-hero.js
@@ -1,10 +1,10 @@
-// <%= componentName %>
+// vf-hero
 
 // Don't need JS? Then feel free to delete this file.
 
 /*
  * A note on the Visual Framework and JavaScript:
- * The VF is primarily a CSS framework so we've included only a minimal amount
+ * The VF is primairly a CSS framework so we've included only a minimal amount
  * of JS in components and it's fully optional (just remove the JavaScript selectors
  * i.e. `data-vf-js-tabs`). So if you'd rather use Angular or Bootstrap for your
  * tabs, the Visual Framework won't get in the way.
@@ -41,4 +41,3 @@
 // // import { vfcomponentName } from '../components/raw/vf-component/vf-component.js';
 // // And, if needed, invoke it
 // // vfcomponentName();
-
